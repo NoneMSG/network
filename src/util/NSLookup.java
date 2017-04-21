918ea5a23313ab5a0bef7fac1ed64cf8555d1d2c
bit-user <bit-user@bit>
2017-04-21 16:34:45 +0900

    edited little bit and add some annotations

--- src/util/NSLookup.java
+++ src/util/NSLookup.java
@@ -9,41 +9,42 @@ public class NSLookup {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		
 			while(true){
 				System.out.print(">>");
 				String ipAD = sc.next();
+				
 				if("exit".equals(ipAD)){
 					System.out.println("eixt");
+					//System.exit(0);
 					break;
 				}else{
 					try {
 						InetAddress[] inetAddress = InetAddress.getAllByName(ipAD);
-						
-						byte[] addresses=null;
-						
+						String stringAdd = null;
+						//byte[] addresses=null;
 						for(int i = 0 ; i < inetAddress.length; ++i){
-							addresses = inetAddress[i].getAddress();
+							//addresses = inetAddress[i].getAddress();
+							stringAdd=inetAddress[i].getHostAddress();
 						}
 						System.out.print(ipAD+" : ");
-						for(int i = 0 ; i < addresses.length ; ++i){
-							int ads = addresses[i] & 0x000000ff;
-							System.out.print(ads);
-							if(i<3){
-								System.out.print(".");	
-							}
-						}
-						System.out.println();
+						System.out.println(stringAdd);
+						
+//						System.out.print(ipAD+" : ");
+//						for(int i = 0 ; i < addresses.length ; ++i){
+//							int ads = addresses[i] & 0x000000ff;
+//							System.out.print(ads);
+//							if(i<3){
+//								System.out.print(".");	
+//							}
+//						}
+						//System.out.println();
+						
 					} catch (UnknownHostException e) {
-	
-						e.printStackTrace();
+						System.out.println("a");
 					}
 				}
 			}
-			
-			
-			
-		
+			sc.close();
 
 	}
 
